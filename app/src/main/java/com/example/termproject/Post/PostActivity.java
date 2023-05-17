a836debf0a72d88e7c26b594a4f12275a8147b0d
DESKTOP-VK7B997\young_ll2sf8f <younge0904@gmail.com>
2023-05-17 13:43:44 +0900

    게시물 작성 UI작성 post_activity.xml PostActivity.java

---
+++ app/src/main/java/com/example/termproject/Post/PostActivity.java
@@ -0,0 +1,16 @@
+package com.example.termproject.Post;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.termproject.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_activity);
+    }
+}
