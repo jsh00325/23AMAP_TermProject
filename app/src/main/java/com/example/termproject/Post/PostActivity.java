aa73dd510201482c81fd95470c17d024b2aa4b5d
Jeong SuHyeon <jsh00325@naver.com>
2023-05-24 21:16:05 +0900

    로그인 기능 구현

--- app/src/main/java/com/example/termproject/Post/PostActivity.java
+++ app/src/main/java/com/example/termproject/Post/PostActivity.java
@@ -2,15 +2,60 @@ package com.example.termproject.Post;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.termproject.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class PostActivity extends AppCompatActivity {
+
+    EditText hashTag, contents;
+    Button upload;
+    DocumentReference userRef;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_activity);
+
+//        hashTag = findViewById(R.id.hashTag);
+//        contents = findViewById(R.id.contents);
+//
+//        upload = findViewById(R.id.upload);
+        upload.setOnClickListener(onClickListener);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.imageButton2) {
+                new AlertDialog.Builder(PostActivity.this)
+                        .setTitle("알람 팝업")
+                        .setMessage("게시하시겠습니까?")
+                        .setPositiveButton("아니요", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        })
+                        .setNegativeButton("네", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        })
+                        .show();
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/termproject/UserManagement/LoginActivity.java
+++ app/src/main/java/com/example/termproject/UserManagement/LoginActivity.java
@@ -6,88 +6,79 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.example.termproject.R;
-/*
-import com.example.termproject.YoonJinTestActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.example.termproject.MainActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-*/
+
+import com.example.termproject.R;
+
 
 public class LoginActivity extends AppCompatActivity {
     // private FirebaseAuth mAuth;
     private static final String TAG = "LoginActivity";
+    private TextView id;
+    private TextView password;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        // Initialize Firebase Auth
-        // mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.loginButton).setOnClickListener(mOnClickListener);
-        findViewById(R.id.gotopasswordResetButton).setOnClickListener(mOnClickListener);
-        findViewById(R.id.goToSignUp).setOnClickListener(mOnClickListener);
-    }
+        id = findViewById(R.id.idEditText_login);
+        password = findViewById(R.id.passwordEditText_login);
 
+        findViewById(R.id.loginButton).setOnClickListener(view -> {
+            // 사용자가 입력한 아이디와 비밀번호 가져오기
 
-    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            String inputId = id.getText().toString().trim();
+            String inputPassword = password.getText().toString().trim();
 
-        @Override
-        public void onClick(View view) {
-            Intent intent;
-            int id = view.getId();
-            if (id == R.id.loginButton) {
-                login();
-            } else if (id == R.id.gotopasswordResetButton) {// TODO : 액티비티 만들어서 연결해두기
-                // intent = new Intent(LoginActivity.this, PasswordResetActivity.class);
-                // startActivity(intent);
-            } else if (id == R.id.goToSignUp) {
-                intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
+            // 이메일 형식인지 확인
+            if (!isEmailValid(inputId)) {
+                Toast.makeText(LoginActivity.this, "유효한 이메일을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                return; // 유효하지 않은 이메일인 경우, 함수를 종료하고 다시 입력받도록 합니다.
             }
-        }
-    };
 
-    private void login() {
-        String email = ((EditText) findViewById(R.id.emailEditText_login)).getText().toString();
-        String password = ((EditText) findViewById(R.id.passwordEditText_login)).getText().toString();
-        /*
-        if (email.length() > 0 && password.length() > 0) {
-
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-
+            // 아이디와 비밀번호 유효성 검사
+            if (inputId.isEmpty() || inputPassword.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+            }  else {
+                // Firebase Authentication을 사용한 로그인 처리
+                mAuth.signInWithEmailAndPassword(inputId, inputPassword)
+                        .addOnCompleteListener(this, task -> {
                             if (task.isSuccessful()) {
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Toast.makeText(LoginActivity.this,"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-
+                                // 로그인 성공
+                                Toast.makeText(LoginActivity.this, "로그인이 완료되었습니다.", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                                 startActivity(intent);
-
+                                finish();
                             } else {
-                                if (task.getException() != null) {
-                                    Toast.makeText(LoginActivity.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
-                                }
+                                // 로그인 실패
+                                Toast.makeText(LoginActivity.this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
                             }
-                        }
-                    });
-        } else {
-            Toast.makeText(LoginActivity.this,"이메일 또는 비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
-        }
-        */
+                        });
+            }
+        });
+
+        findViewById(R.id.gotopasswordResetButton).setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, PasswordResetActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.goToSignUp).setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
     }
 
-    private void toast(String text){
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
     }
 }
