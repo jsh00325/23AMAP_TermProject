c9513a981d60098158b8d781740dd50e77227142
ohje <ohjs87968628@gmail.com>
2023-05-27 23:56:32 +0900

    이메일 인증 기능 추가 LoginActivity.java & SignUpActivity.java 수정

--- app/src/main/java/com/example/termproject/UserManagement/SignUpActivity.java
+++ app/src/main/java/com/example/termproject/UserManagement/SignUpActivity.java
@@ -3,7 +3,9 @@ package com.example.termproject.UserManagement;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -79,38 +81,86 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
+    private boolean isValidEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
     private void signUp(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("이메일을 입력하세요.");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (!isValidEmail(email)) {
+            emailEditText.setError("유효한 이메일을 입력하세요.");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("비밀번호를 입력하세요.");
+            passwordEditText.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            passwordEditText.setError("비밀번호는 최소 6자 이상이어야 합니다.");
+            passwordEditText.requestFocus();
+            return;
+        }
+
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // 이메일 인증 보내기
+                            // 회원가입 성공
+                            Toast.makeText(getApplicationContext(), "이메일로 인증을 완료해주세요.", Toast.LENGTH_SHORT).show();
                             sendEmailVerification();
                         } else {
-                            // 회원 가입 실패
-                            Toast.makeText(SignUpActivity.this, "회원 가입에 실패했습니다.",
-                                    Toast.LENGTH_SHORT).show();
+                            // 회원가입 실패
+                            Toast.makeText(getApplicationContext(), "회원가입에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
+    // 이메일 인증 메일 전송
     private void sendEmailVerification() {
         FirebaseUser user = mAuth.getCurrentUser();
-        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(SignUpActivity.this, "이메일 인증을 보냈습니다. 이메일을 확인해주세요.",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Log.e("TAG", "sendEmailVerification", task.getException());
-                    Toast.makeText(SignUpActivity.this, "이메일 인증 보내기에 실패했습니다.",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // 이메일 인증 메일 전송 성공
+                            Toast.makeText(getApplicationContext(), "이메일로 인증 링크가 전송되었습니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 이메일 인증 메일 전송 실패
+                            Toast.makeText(getApplicationContext(), "이메일 전송에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "회원가입에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // 이메일 인증 완료 시 회원 등록 처리
+    private void registerUserAfterEmailVerification() {
+        // 현재 사용자 가져오기
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        // 사용자가 있고 이메일 인증이 완료된 경우에만 회원 등록 처리
+        if (user != null && user.isEmailVerified()) {
+            // 회원 등록 처리 코드 작성
+            // 파이어베이스 user에 사용자 등록
+            // ...
+
+            Toast.makeText(getApplicationContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "이메일 인증을 완료해주세요.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // onStart() - 기존 사용자 관련 안 함.
