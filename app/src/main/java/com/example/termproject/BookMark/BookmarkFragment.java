5a3dd3efb028fdc12dfe7acd872ac6fa941ad90f
ohje <ohjs87968628@gmail.com>
2023-06-03 20:36:43 +0900

    bookmark 컽

--- app/src/main/java/com/example/termproject/BookMark/BookmarkFragment.java
+++ app/src/main/java/com/example/termproject/BookMark/BookmarkFragment.java
@@ -1,68 +1,190 @@
 package com.example.termproject.BookMark;
 
-//파이어베이스 사용하기에 일단 안 건드림
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.termproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BookmarkFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BookmarkFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerView;
+    Context mContext;
+    BookmarkRecyclerViewAdapter bookmarkRecyclerViewAdapter;
+    private FirebaseUser user;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentSnapshot userDoc;
+    DocumentReference userRef;
+    Map<String, List<String>> bookMark = new HashMap<>();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ArrayList<BookmarkItem> bookmarkItemList = new ArrayList<>();
+    int tenCnt = 0;
 
     public BookmarkFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookmarkFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BookmarkFragment newInstance(String param1, String param2) {
-        BookmarkFragment fragment = new BookmarkFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.bookmark_fragment, container, false);
+        View view = inflater.inflate(R.layout.bookmark_fragment, container, false);
+        mContext = container.getContext();
+        bookMark.clear();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView = view.findViewById(R.id.fragment_bookmark_recyclerview);
+        recyclerView.setHasFixedSize(true); //기존 성능 강화
+
+        db.collection("users").document(user.getUid()).get().
+                addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            userDoc = task.getResult();
+                            userRef = userDoc.getReference();
+                            if(!userDoc.exists()){
+                                db.collection("club_list").document(user.getUid()).get()
+                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if(task.isSuccessful()){
+                                                    Log.d("user", "동아리입니다");
+                                                    userDoc = task.getResult();
+                                                    userRef = userDoc.getReference();
+                                                    getBookMarkClubList();
+                                                }
+                                            }
+                                        });
+                            }
+                            else {
+                                Log.d("user", "유저입니다");
+                                getBookMarkClubList();
+                            }
+                        }
+                        else Log.d("task", "실패");
+                    }
+                });
+
+        //굳이 안 적어도 되는 코드, 나중에 지울게
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+
+    public void getBookMarkClubList(){
+        bookMark = (Map<String, List<String>>) userDoc.get("bookMark");
+        bookmarkItemList.clear();
+        ArrayList<String> clubList = new ArrayList<>();
+        for(String minor : bookMark.keySet()){
+            List<String> minorList = bookMark.get(minor);
+            for(String club : minorList){
+                clubList.add(club);
+            }
+        }
+        Log.d("user", "여기까진 왔다..1");
+        tenCnt = clubList.size() / 10;
+        if(clubList.size() % 10 == 0) tenCnt--;
+        Log.d("user", "여기까지 왔다...3");
+
+        if(tenCnt > 0) {
+            for (int i = 1; i <= tenCnt; i++) {
+                List<String> now = clubList.subList(10 * (i - 1), 10 * i - 1);
+                db.collection("club_list").whereIn("club_name", now).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        bookmarkItemList.add(new BookmarkItem(
+                                                (String) document.get("image_url"), (String) document.get("club_name"),
+                                                (String) document.get("main_category"), (String) document.get("sub_category"), document.getReference()));
+                                    }
+                                }
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (bookmarkItemList.size() == tenCnt * 10) {
+                                    List<String> now = clubList.subList(10 * tenCnt, clubList.size());
+                                    db.collection("club_list").whereIn("club_name", now).get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                                            bookmarkItemList.add(new BookmarkItem(
+                                                                    (String) document.get("image_url"), (String) document.get("club_name"),
+                                                                    (String) document.get("main_category"), (String) document.get("sub_category"),
+                                                                    document.getReference()));
+
+                                                            if(bookmarkItemList.size() == clubList.size()){
+                                                                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                                                                bookmarkRecyclerViewAdapter = new BookmarkRecyclerViewAdapter(bookmarkItemList, userRef, mContext);
+                                                                recyclerView.setAdapter(bookmarkRecyclerViewAdapter);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+                        });
+            }
+        }
+        else if(clubList.size() > 0){
+            List<String> now = clubList.subList(0, clubList.size());
+            db.collection("club_list").whereIn("club_name", now).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    bookmarkItemList.add(new BookmarkItem(
+                                            (String) document.get("image_url"), (String) document.get("club_name"),
+                                            (String) document.get("main_category"), (String) document.get("sub_category"),
+                                            document.getReference()));
+
+                                    if(bookmarkItemList.size() == clubList.size()){
+                                        Log.d("user", "여기까지 왔다...2");
+                                        Log.d("user", String.valueOf(clubList));
+                                        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                                        bookmarkRecyclerViewAdapter = new BookmarkRecyclerViewAdapter(bookmarkItemList, userRef, mContext);
+                                        recyclerView.setAdapter(bookmarkRecyclerViewAdapter);
+                                    }
+                                }
+                            }
+                        }
+                    });
+        }
+        else{
+
+        }
     }
 }
